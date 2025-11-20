4cb377c6b82b2aed93999cdd676e30e8285b0bf3
6Morpheus6 <wanton-decay@gmx.net>
2025-11-20 19:33:11 +0100

    update
    - use .venv
    - remove comments

--- install.js
+++ install.js
@@ -1,6 +1,8 @@
 module.exports = {
+  requires: {
+    bundle: "ai"
+  },
   run: [
-    // Edit this step to customize the git repository to use
     {
       method: "shell.run",
       params: {
@@ -9,31 +11,29 @@ module.exports = {
         ]
       }
     },
-    // Delete this step if your project does not use torch
+    {
+      method: "shell.run",
+      params: {
+        venv_python: "3.12",
+        venv: ".venv",
+        path: "app",
+        message: [
+          "uv sync"
+        ]
+      }
+    },
     {
       method: "script.start",
       params: {
         uri: "torch.js",
         params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
-          // triton: true   // uncomment this line if your project requires triton
-          // sageattention: true   // uncomment this line if your project requires sageattention
+          venv: ".venv",
+          path: "app",
+          // xformers: true
+          // triton: true
+          // sageattention: true
         }
       }
-    },
-    // Edit this step with your custom install commands
-    {
-      method: "shell.run",
-      params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        message: [
-          "uv pip install gradio devicetorch",
-          "uv pip install -r requirements.txt"
-        ]
-      }
-    },
+    }
   ]
 }
